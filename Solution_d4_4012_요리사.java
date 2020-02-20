8081345b5615a0563655132b914cc763fbdb9ecb
alsgh4098 <41179076+alsgh4098@users.noreply.github.com>
2020-02-20 18:04:29 +0900

    Add files via upload

--- Solution_d4_4012_요리사.java
+++ Solution_d4_4012_요리사.java
@@ -1,109 +1,78 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Solution_d4_4012_요리사 {
 	
-	static int[][] map;
 	static int N;
-	
-	static int size;
-	
-	static int[] nums;
-	
-	static int[] res;
-	
-	static int min;
+	static int[][] map;
+	static int[] arr;
+	static int min = Integer.MAX_VALUE;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
-		int TC = Integer.parseInt(br.readLine());
+		int TC = sc.nextInt();
 		
 		for (int t = 1; t <= TC; t++) {
-			N = Integer.parseInt(br.readLine());
-			
-			StringTokenizer st;
-			
-			min = Integer.MAX_VALUE;
+			N = sc.nextInt();
 			
 			map = new int[N][N];
-			
-			res = new int[2];
-			
-			size = (N*N-N)/2;
-			
-			nums = new int[size];
-			
-			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < N; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-			
-			int n = 0;
+			arr = new int[N];
+			min = Integer.MAX_VALUE;
 			
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					if(i < j) {
-						nums[n] = map[i][j] + map[j][i];
-						n++;
-					}
+					map[i][j] = sc.nextInt();
 				}
 			}
 			
-			for (int i = 0; i < nums.length; i++) {
-				System.out.println(nums[i]);
-			}
-			
-			combi(0,0);
+			solution(0,0);
 			
 			System.out.printf("#%d %d\n",t,min);
-			System.out.println();
 			
+		}
+
+	}
+	
+	public static void solution(int start, int count) {
+		if(count == N/2 ) {
+			int res = solution2();
 			
-//			printMap(map);
-			
-			
-			
+			if( res < min) {
+				min = res;
+			}
+			return;
 		}
 		
-
-	}
-	
-	static void printMap(int[][] map){
-		for (int i = 0; i < map.length; i++) {
-			for (int j = 0; j < map.length; j++) {
-				System.out.print(map[i][j]+" ");
-			}
-			System.out.println();
-		}
-	}
-
-	
-	static void combi(int start, int count) {
-		if(count == 2) {
-			
-			int sum = Math.abs( res[0] - res[1] );
-			
-			System.out.println("0      "+res[0]);
-			
-			System.out.println("1      "+res[1]);
-			
-			if(  sum < min ) {
-				min = sum;
-				System.out.println("minmin"+min);
-			}
-			
-			return;
-		}else {
-			for (int i = start; i < size; i++) {
-				res[count] = nums[i];
-				combi(start+1,count+1);
+		for (int i = start; i < N; i++) {
+			if(arr[i] != 1) {
+				arr[i] = 1;
+				solution(i+1,count+1);
+				arr[i] = 0;
 			}
 		}
+	}
+	
+	public static int solution2() {
+		int sum1 = 0;
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if( arr[i]==1 && arr[j] ==1) {
+					sum1 += map[i][j];
+				}
+			}
+		}
+		
+		int sum2 = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if( arr[i]!=1 && arr[j] !=1) {
+					sum2 += map[i][j];
+				}
+			}
+		}
+		
+		return Math.abs(sum1-sum2);
 	}
 
 }
